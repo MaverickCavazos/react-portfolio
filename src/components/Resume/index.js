dc29cbaaea54d3373f259b6bd2a1b7545407f9f5
Maverick <Mav33987@gmail.com>
2022-03-28 17:36:33 -0500

    finished out the contact form, add button to show resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,10 +3,13 @@ import React from 'react';
 function Resume() {
     return (
         <header className="section-header" >
-            <h2 className="section-h2">Download my Resume!</h2>
+            <h2 className="section-h2">Click the button to see my resume!</h2>
             <p>
-                Resume page
+                This link will give you access to my resume and provide you with a snapchost of my work history, proficiencies, and education!
             </p>
+            <form action="https://docs.google.com/document/d/1A_GH68zxbWGciIBp--g8pi9mwNPx_LtuXG-54iallSo/edit?usp=sharing">
+         <button className="button" type="submit">See Resume</button>
+      </form>
         </header>
     );
 }
